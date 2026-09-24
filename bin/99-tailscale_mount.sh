46828d685e28369b177676c33d974d0fefc49424
tekkamelon <type2guntank@gmail.com>
2026-09-24 17:46:19 +0900

    feat(bin): tailscaleでの自動マウントの設定を変更

--- bin/99-tailscale_mount.sh
+++ bin/99-tailscale_mount.sh
@@ -1,51 +1,13 @@
-#!/bin/sh 
+#!/bin/sh
+# 自宅 LAN に直結している時だけ tailscale down、それ以外は up。
+# NAS は名前 rei でマウントし解決先が場所で変わるため、切替時に必ず張り直す。
+set -u
 
-# このスクリプトを"/etc/NetworkManager/dispatcher.d/"以下のディレクトリに配置
-# hogeを適宜除外するAP名に設定
+route=$(ip route get 192.168.1.145 2>/dev/null | head -1)
+case "${route}" in
+  *"dev tailscale0"*) tailscale up ;;    # サブネットルート経由 = 外出先
+  *" via "*)          tailscale up ;;    # 他ネットワークのGW経由 = 外出先
+  *)                  tailscale down ;;  # LAN 直結 = 自宅
+esac
 
-set -eu
-
-# 除外するAP
-home_ap=hoge
-
-# 接続先のAP
-ap_name=$(
-
-	iwconfig 2> /dev/null |
-
-	# 区切り文字を":"とスペースに指定
-	awk -F[:" "] '
-
-	# 1行目のみを処理
-	NR == 1{
-
-		# 6フィールド目が"802.11"かつ9フィールド目が"off/any"ではない場合に真
-		if($6 == "802.11" && $9 != "off/any"){
-
-			# AP名を出力
-			print $9
-
-		}else{
-
-			print "no_wireless"
-
-		}
-
-	}
-	'
-)
-
-# wi-fiに接続されていないか"home_ap"に接続されている場合に真
-if [ "${ap_name}" = "no_wireless" ] || echo "${ap_name}" | grep -F -q "${home_ap}" ; then
-
-	tailscale down
-
-# "home_ap"以外のwi-fiに接続されている場合に真
-elif echo "${ap_name}" | grep -F -v -q "${home_ap}" ; then
-
-	tailscale up &&
-
-	# 再マウント
-	mount -a
-fi
-
+exit 0
